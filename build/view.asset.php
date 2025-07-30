92c583f3fb0072ae26ce52e0c582001bea2d6d2d
Md. Merajul Islam Sojib <sojibhasan9878@gmail.com>
2025-07-30 17:55:05 +0600

    some changes

--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-i18n'), 'version' => '3521901f186b36319d61');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-i18n'), 'version' => 'abd33a7f258a877eb242');
